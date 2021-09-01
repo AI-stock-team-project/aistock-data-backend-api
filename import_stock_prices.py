f7529102cf4eb75d11280a6de868b64f45630b07
SH.H <e2xist@gmail.com>
2021-09-01 10:44:47 +0900

    import 소스 커밋.

--- import_stock_prices.py
+++ import_stock_prices.py
@@ -11,21 +11,54 @@
 import aistock.database as aistock_database
 import pandas as pd
 from pandas import DataFrame
-from aistock import stock_prices_sqlite as sqlite_table
 from aistock.StockPrice import StockPriceTable
+import aistock.stock_prices_sqlite as stock_prices_sqlite
+from aistock.stock_prices_sqlite import StockPriceSQLiteTable as StockPriceSQLiteTable
 import time
 from datetime import timedelta
+from sqlalchemy import create_engine
+import os
 
 
-FROM_TABLE = sqlite_table.StockPriceTable
+# FROM_TABLE = sqlite_table.StockPriceSqliteTable2
+SQLITE_PATH = 'stock_prices.db'  # 기본값임. 변경 가능.
+
+
+def get_engine():
+    engine = create_engine(f'sqlite:///{SQLITE_PATH}', echo=True)
+    return engine
+
+
+def import_old_data():
+    # 데이터베이스를 조회해서, row 가 없는 상태라면 sqlite 로 import 를 시행한다.
+    if count_mysql_table() == 0:
+        # 작업할 연도
+        years = [
+            '2016', '2017', '2018', '2019', '2020'
+        ]
+        global SQLITE_PATH
+        back_sqlite_path = SQLITE_PATH
+        for year in years:
+            start = time.time()
+            SQLITE_PATH = f'stock_prices_{year}.db'
+            if not os.path.exists(SQLITE_PATH):
+                print("[import_old_data] 파일이 없음. - " + SQLITE_PATH)
+            else:
+                import_from_sqlite()
+            print(f"[import_stock_price] {year} [", timedelta(seconds=(time.time() - start)), ']')
+
+        # 원래의 기본값으로 되돌려놓음
+        SQLITE_PATH = back_sqlite_path
+    else:
+        print("[import_stock_price] 데이터가 있는 상태이므로 주가 정보 import를 진행하지 않음")
 
 
 def load_from_sqlite() -> DataFrame:
     """
     sqlite 파일에서 읽어와서 mysql에 데이터를 넣는다.
     """
-    df = pd.read_sql(f'select * from {FROM_TABLE.__tablename__}',
-                     con=sqlite_table.get_engine())
+    df = pd.read_sql(f'select * from {StockPriceSQLiteTable.__tablename__}',
+                     con=get_engine())
     return df
 
 
@@ -37,15 +70,15 @@ def import_from_sqlite():
     df = load_from_sqlite()
     df.rename(
         columns={
-            FROM_TABLE.symbol: StockPriceTable.symbol.name,
-            FROM_TABLE.date: StockPriceTable.date.name,
-            FROM_TABLE.open: StockPriceTable.open.name,
-            FROM_TABLE.high: StockPriceTable.high.name,
-            FROM_TABLE.low: StockPriceTable.low.name,
-            FROM_TABLE.close: StockPriceTable.close.name,
-            FROM_TABLE.volume: StockPriceTable.volume.name,
-            FROM_TABLE.trad_value: StockPriceTable.trad_value.name,
-            FROM_TABLE.fluc_rate: StockPriceTable.fluc_rate.name
+            StockPriceSQLiteTable.symbol.name: StockPriceTable.symbol.name,
+            StockPriceSQLiteTable.date.name: StockPriceTable.date.name,
+            StockPriceSQLiteTable.open.name: StockPriceTable.open.name,
+            StockPriceSQLiteTable.high.name: StockPriceTable.high.name,
+            StockPriceSQLiteTable.low.name: StockPriceTable.low.name,
+            StockPriceSQLiteTable.close.name: StockPriceTable.close.name,
+            StockPriceSQLiteTable.volume.name: StockPriceTable.volume.name,
+            StockPriceSQLiteTable.trad_value.name: StockPriceTable.trad_value.name,
+            StockPriceSQLiteTable.fluc_rate.name: StockPriceTable.fluc_rate.name
         },
         inplace=True
     )
@@ -82,6 +115,9 @@ if __name__ == '__main__':
     if count_mysql_table() == 0:
         main_start = time.time()
         print("[import_stock_price] >> ")
+        # 오래 전 데이터도 로드.
+        import_old_data()
+        # 최근 2021년 이후의 데이터 로드.
         import_from_sqlite()
         print("<< [import_stock_price] [", timedelta(seconds=(time.time() - main_start)), ']')
     else:
